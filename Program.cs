3c275fe7c3edb0b45cca84fed4afe7cb68837866
jakiracf <filipj12456@gmail.com>
2023-11-03 17:00:31 +0100

    test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace RUAP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav od Fiće <3");
+            Console.WriteLine("Pozdrav od Fiće <3 6db45efbf209d57302fa9e0a1a4c0e43d3f90385");
         }
     }
 }
